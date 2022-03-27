6a861a9cafd1396fc4553fac19390d6f9baa58f3
Bruno Hubner <brunohubnerdev@gmail.com>
2022-03-27 14:27:19 -0300

    feat: add social icons

---
+++ src/components/SocialLinks/icons.js
@@ -0,0 +1,6 @@
+import { Github } from "styled-icons/boxicons-logos/Github"
+import { Twitter } from "styled-icons/boxicons-logos/Twitter"
+import { Instagram } from "styled-icons/boxicons-logos/Instagram"
+import { Linkedin } from "styled-icons/boxicons-logos/Linkedin"
+
+export default [Github, Linkedin, Twitter, Instagram]
---
+++ src/components/SocialLinks/links.js
@@ -0,0 +1,18 @@
+const links = [
+    {
+        label: "Github",
+        url: `https://github.com/brunohubner`
+    },
+    {
+        label: "LinkedIn",
+        url: `https://linkedin.com/in/brunohubner01`
+    },
+    {
+        label: "Twitter",
+        url: `https://twitter.com/brunohubner01`
+    },
+    {
+        label: "Instagram",
+        url: `https://www.instagram.com/brunohubner01`
+    }
+]
