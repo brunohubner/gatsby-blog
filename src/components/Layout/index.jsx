edc30203ca12d604dcfda2dfa1170f888c89919b
Bruno Hubner <brunohubnerdev@gmail.com>
2022-03-27 13:28:26 -0300

    chore: install styled components

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,15 +1,16 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Profile from "../Profile"
+import * as S from "./styles"
 
 export default function Layout({ children }) {
     return (
-        <>
+        <S.LayoutWrapper>
             <aside>
                 <Profile />
             </aside>
-            <main>{children}</main>
-        </>
+            <S.LayoutMain>{children}</S.LayoutMain>
+        </S.LayoutWrapper>
     )
 }
 
