6d847d82e7653c21ecad03922f0ffc3ab17ddc23
Bruno Hubner <brunohubnerdev@gmail.com>
2022-04-03 15:04:07 -0300

    style: add grid option in bloglist page

--- src/templates/BlogList.jsx
+++ src/templates/BlogList.jsx
@@ -4,6 +4,7 @@ import SEO from "../components/SEO"
 import PostItem from "../components/PostItem"
 import { graphql } from "gatsby"
 import Pagination from "../components/Pagination"
+import * as S from "../components/ListWrapper/styles"
 
 export const query = graphql`
     query PostList($skip: Int!, $limit: Int!) {
@@ -45,32 +46,34 @@ export default function BlogList(props) {
     return (
         <Layout>
             <SEO title="Home" />
-            {postList.map(
-                ({
-                    node: {
-                        frontmatter: {
-                            background,
-                            category,
-                            date,
-                            description,
-                            title
-                        },
-                        timeToRead,
-                        fields: { slug }
-                    }
-                }) => (
-                    <PostItem
-                        key={slug}
-                        slug={slug}
-                        background={background}
-                        category={category}
-                        date={date}
-                        timeToRead={timeToRead}
-                        title={title}
-                        description={description}
-                    />
-                )
-            )}
+            <S.ListWrapper>
+                {postList.map(
+                    ({
+                        node: {
+                            frontmatter: {
+                                background,
+                                category,
+                                date,
+                                description,
+                                title
+                            },
+                            timeToRead,
+                            fields: { slug }
+                        }
+                    }) => (
+                        <PostItem
+                            key={slug}
+                            slug={slug}
+                            background={background}
+                            category={category}
+                            date={date}
+                            timeToRead={timeToRead}
+                            title={title}
+                            description={description}
+                        />
+                    )
+                )}
+            </S.ListWrapper>
             <Pagination
                 isFirst={isFirst}
                 isLast={isLast}
